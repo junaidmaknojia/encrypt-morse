0d88af7e367c15cfa8cb311a9651a955de08194d
junaid <junaidmaknojia786@yahoo.com>
2021-02-03 01:11:59 -0600

    added-onTouchEvent-method

---
+++ app/src/main/java/com/example/encryptmorse/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.encryptmorse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                Log.d("DEBUG_TAG","Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d("DEBUG_TAG","Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d("DEBUG_TAG","Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d("DEBUG_TAG","Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d("DEBUG_TAG","Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
+}
