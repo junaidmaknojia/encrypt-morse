8b15dab4821418db5d7c8da36b0cb3859c4558e3
junaid <junaidmaknojia786@yahoo.com>
2021-02-07 13:36:33 -0600

    tested-commands

--- app/src/main/java/com/example/encryptmorse/MainActivity.java
+++ app/src/main/java/com/example/encryptmorse/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        /**
         preOrder = ["start", "T", "M", "O", "-", "0", "9", "*", "8", "G", "Q", "Z", "7", "N", "K",
                     "Y", "C", "D", "X", "B", "6", "E", "A", "W", "J", "1", "P", "R", "L", "I",
                     "U", "-", "2", "F", "S", "V", "3", "H", "4", "5"];
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         postOrder = ["0", "9", "-", "8", "*", "O", "Q", "7", "Z", "G", "M", "Y", "C", "K", "X", "6",
                     "B", "D", "N", "T", "1", "J", "P", "W", "L", "R", "A", "2", "-", "F", "U", "3",
                     "V", "4", "5", "H", "S", "I", "E", "start"];
+         */
 
     }
 
@@ -66,34 +68,38 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onDown(MotionEvent motionEvent) {
+        Log.d("DEBUG_TAG","onDown");
         return false;
     }
 
     @Override
     public void onShowPress(MotionEvent motionEvent) {
-
+        Log.d("DEBUG_TAG","onShowPress");
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent motionEvent) {
-        //dot
+        Log.d("DEBUG_TAG","onSingleTapUp");
+        //motionEvent.
         return false;
     }
 
     @Override
     public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        //
+        //save
+        Log.d("DEBUG_TAG","onScroll");
         return false;
     }
 
     @Override
     public void onLongPress(MotionEvent motionEvent) {
         //dash
-
+        Log.d("DEBUG_TAG","Dash / onLongPress");
     }
 
     @Override
     public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        Log.d("DEBUG_TAG","onFling");
         return false;
     }
 }
