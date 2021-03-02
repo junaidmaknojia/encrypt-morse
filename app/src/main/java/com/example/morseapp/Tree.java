1d1546b91d1dae6d50625603a46daf9bacfb309c
junaid <junaidmaknojia786@yahoo.com>
2021-03-01 23:56:31 -0600

    added search code

--- app/src/main/java/com/example/morseapp/Tree.java
+++ app/src/main/java/com/example/morseapp/Tree.java
@@ -12,6 +12,27 @@ public class Tree {
         }else if(direction == "right"){
             // return val.right;
         }
+//        searchRecur(val, currRoot = this.root){
+//            if (!this.root) return false;
+//
+//            if (val === currRoot.val) {
+//                return true;
+//            } else if (val > currRoot.val) {
+//                if (currRoot.right) {
+//                    currRoot = currRoot.right;
+//                    return this.searchRecur(val, currRoot)
+//                }
+//
+//                return false
+//            } else if (val < currRoot.val){
+//                if (currRoot.left) {
+//                    currRoot = currRoot.left;
+//                    return this.searchRecur(val, currRoot)
+//                }
+//
+//                return false;
+//            }
+//        }
     }
 }
 
