6ffbcdf067699e2229b5f679b8fa76039722896b
junaid <junaidmaknojia786@yahoo.com>
2021-03-01 23:39:27 -0600

    created tree class to navigate morse tree

---
+++ app/src/main/java/com/example/morseapp/Tree.java
@@ -0,0 +1,16 @@
+package com.example.morseapp;
+
+public class Tree {
+
+    public Tree() {
+        Object val;
+    }
+
+    private void searchTree(String direction){
+        if(direction == "left"){
+//            return val.left;
+        }else if(direction == "right"){
+            // return val.right;
+        }
+    }
+}
