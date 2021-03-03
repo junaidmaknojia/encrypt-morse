1373ffaaf0de7d1e0fcb8b60e61d28d92f17a871
junaid <junaidmaknojia786@yahoo.com>
2021-03-02 23:02:31 -0600

    switched to gesture set of events

--- app/src/main/java/com/example/morseapp/MainActivity.java
+++ app/src/main/java/com/example/morseapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.morseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.MotionEventCompat;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener{
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,39 +31,104 @@ public class MainActivity extends AppCompatActivity {
              "B", "D", "N", "T", "1", "J", "P", "W", "L", "R", "A", "2", "-", "F", "U", "3",
              "V", "4", "5", "H", "S", "I", "E", "start"];
          */
+        mDetector = new GestureDetectorCompat(this, this);
+        mDetector.setOnDoubleTapListener(this);
     }
 
     public boolean onTouchEvent(MotionEvent event){
-
-        int action = MotionEventCompat.getActionMasked(event);
-        TextView text = findViewById(R.id.textView);
-        switch(action) {
-            case (MotionEvent.ACTION_DOWN) :
-                Log.d("DEBUG_TAG","Action was DOWN");
-//                text.setText(R.string.screenText);
-                if(text != null){
-                    text.setText("Down");
-                }
-                return true;
-            case (MotionEvent.ACTION_MOVE) :
-                Log.d("DEBUG_TAG","Action was MOVE");
-                return true;
-            case (MotionEvent.ACTION_UP) :
-                Log.d("DEBUG_TAG","Action was UP");
-                if(text != null){
-                    text.setText("Up");
-                }
-                return true;
-            case (MotionEvent.ACTION_CANCEL) :
-                Log.d("DEBUG_TAG","Action was CANCEL");
-                return true;
-            case (MotionEvent.ACTION_OUTSIDE) :
-                Log.d("DEBUG_TAG","Movement occurred outside bounds " +
-                        "of current screen element");
-                return true;
-            default :
-                return super.onTouchEvent(event);
+        if(this.mDetector.onTouchEvent(event)) {
+            return true;
         }
+//        int action = MotionEventCompat.getActionMasked(event);
+//        TextView text = findViewById(R.id.textView);
+//        switch(action) {
+//            case (MotionEvent.ACTION_DOWN) :
+//                Log.d("DEBUG_TAG","Action was DOWN");
+////                text.setText(R.string.screenText);
+//                if(text != null){
+//                    text.setText("Down");
+//                }
+//                return true;
+//            case (MotionEvent.ACTION_MOVE) :
+//                Log.d("DEBUG_TAG","Action was MOVE");
+//                return true;
+//            case (MotionEvent.ACTION_UP) :
+//                Log.d("DEBUG_TAG","Action was UP");
+//                if(text != null){
+//                    text.setText("Up");
+//                }
+//                return true;
+//            case (MotionEvent.ACTION_CANCEL) :
+//                Log.d("DEBUG_TAG","Action was CANCEL");
+//                return true;
+//            case (MotionEvent.ACTION_OUTSIDE) :
+//                Log.d("DEBUG_TAG","Movement occurred outside bounds " +
+//                        "of current screen element");
+//                return true;
+//            case (MotionEvent.ACTION_SCROLL) :
+//                Log.d("DEBUG_TAG","Action was Scroll");
+//                if(text != null){
+//                    text.setText("Scroll/Flick");
+//                }
+//                return true;
+//            default :
+                return super.onTouchEvent(event);
+        //}
+
+
     }
 
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
 }
